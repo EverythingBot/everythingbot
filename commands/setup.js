d8963bfd3c801a16c62ee0bdab4cd5ab8c39d4df
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-19 14:45:42 -0400

    Fixed setup
    By fixed, I mean actually made it work...
    Again, inexperienced with MongoDB, but I'm figuring it out

--- commands/setup.js
+++ commands/setup.js
@@ -1,4 +1,6 @@
-exports.run = async function (client, message, args, mongo) {
+exports.run = async function(client, message, args, mongo) {
+
+  var ServerURL = process.env.SERVER;
 
   if (message.member.hasPermission("ADMINISTRATOR")) {
     setup(message, message.author.tag);
@@ -8,81 +10,65 @@ exports.run = async function (client, message, args, mongo) {
 
   function setup(message, author) {
     message.reply("please reply with your welcome channel").then(message => {
-      const filter = m => m.author.tag.includes(author);
-      message.channel.awaitMessages(filter, {
-          max: 1,
-          time: 60000,
-          errors: ['time']
-        })
-        .then(collected => {
-          setupChannel(collected, message, author);
-        })
-        .catch(collected => {
-          if (collected.size < 1)
-            message.channel.send("Setup cancelled, you took longer than 1 minute!");
-        });
-    });
-  }
+        const filter = m => m.author.tag.includes(author);
+        message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 60000,
+            errors: ['time']
+          })
+          .then(collected => {
+              var done = false;
 
-  function setupChannel(collected, message, author) {
-    var query = {
-      "content": -1
-    };
-    var c = collected.first().content.toString().replace(/[<#>]/g, '');
-    var x = collected.first().content;
-    if (message.guild.channels.get(c)) {
-      mongo.connect(ServerURL, function(err, db) {
-        var dbo = db.db("servers");
-        var query = {
-          "serverID": message.guild.id
-        };
-        dbo.collection("servers").findOne(query, function(err, result) {
-          if (err) throw err;
-          var r = result;
-          r.welcomeChannel = c;
-          dbo.collection("servers").update(query, r, function(err, res) {
-            if (err) throw err;
-            message.channel.send("Now send the name of the role you want people to get when they join").then(message => {
-              const filter2 = m => m.author.tag.includes(author);
-              message.channel.awaitMessages(filter2, {
-                  max: 1,
-                  time: 60000,
-                  errors: ['time']
-                })
-                .then(c => {
-                  //console.log(c.first().content);
-                  var role = c.first().content.toString();
-                  //console.log(message.channel.guild.roles.exists("name", role));
-                  if (message.channel.guild.roles.exists("name", role)) {
-                    r.welcomeRole = role;
-                    var dbo = db.db("servers");
-                    var query = {
-                      "serverID": message.guild.id
-                    };
-                    dbo.collection("servers").update(query, r, function(err, result) {
-                      if (err) throw err;
-                      message.channel.send(`Guild default role set to ${role}`);
-                      message.channel.send("Setup complete! (For now)");
-                      db.close();
+              var query = {
+                "serverID": message.guild.id
+              };
+              var c1 = collected.first().content.toString().replace(/[<#>]/g, '');
+              var x = collected.first().content;
+              if (message.guild.channels.get(c1)) {
+                //ser.welcomeChannel = c;
+                message.channel.send("Now send the name of the role you want people to get when they join").then(message => {
+                  const filter2 = m => m.author.tag.includes(author);
+                  message.channel.awaitMessages(filter2, {
+                      max: 1,
+                      time: 60000,
+                      errors: ['time']
+                    })
+                    .then(c => {
+                      //console.log(c.first().content);
+                      var role = c.first().content.toString();
+                      //console.log(message.channel.guild.roles.exists("name", role));
+                      if (message.channel.guild.roles.exists("name", role)) {
+                      //  ser.welcomeRole = role;
+                        message.channel.send('Guild default role set to `' + role + '`');
+                        message.channel.send(`Guild welcome channel set to ${x}`);
+                        message.channel.send("Setup complete! (For now)");
+                        var ser =  { $set: {  "welcomeRole": role,  "welcomeChannel": c1  }  };
+                        mongo.connect(ServerURL, function(err, db) {
+                          var dbo = db.db("servers");
+                          dbo.collection("servers").updateOne(query, ser, function(err, result) {
+                            if (err)
+                              console.log(err);
+                            db.close();
+                          });
+                        });
+                      } else {
+                        message.channel.send("That's not a valid role!");
+                      }
+                    })
+                    .catch(c => {
+                      if (c.size < 1) {
+                        message.channel.send("Setup cancelled, you took longer than 1 minute!");
+                      }
                     });
-                  } else {
-                    message.channel.send("That's not a valid role!");
-                    db.close();
-                  }
-                })
-                .catch(c => {
-                  if (c.size < 1) {
-                    message.channel.send("Setup cancelled, you took longer than 1 minute!");
-                    db.close();
-                  }
                 });
+              } else {
+                message.channel.send("That wasn't a channel! Are you sure you said a channel name? (Ex. `#general`)");
+              }
             });
-          });
-        });
+          })
+      .catch(collected => {
+        if (collected.size < 1)
+          message.channel.send("Setup cancelled, you took longer than 1 minute!");
       });
-      message.channel.send(`Guild welcome channel updated to ${x}`);
-    } else {
-      message.channel.send("That wasn't a channel! Are you sure you said a channel name? (Ex. `#general`)");
     }
-  }
 }
