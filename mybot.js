dd4271e0915d7c2f3487a31cdbc88c6a9bd1c144
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-07-28 21:35:11 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -51,7 +51,7 @@ var helpMenu = {
     description: "EverythingBot, does literally everything (Still in production, currently doesn't do much). Here's the list of commands",
     fields: [{
       name: ":straight_ruler:  Admin/Mod",
-      value: "clear, kick, ban, unban, mute, unmute, setprefix, setup, disable"
+      value: "clear, kick, ban, unban, mute, unmute, setprefix, setup, disable, lockdown"
     },
     {
       name: ":camera:  Image commands",
